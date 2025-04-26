76dfe98a7fe6450cf5570b5fdb953f4ebaa5f51e
0000 <user7210@proton.me>
2025-04-26 20:22:40 +0000

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('app', () => ({
-        currentPage: window.location.pathname.split('/').pop().replace('.html', '') || 'index',
-        activeTool: 'slimfetch',
-        setActiveTool(tool) {
-            this.activeTool = tool;
-        }
-    }));
-});
